7f8630507c189c14f15929c531bcb98e501ad7af
ian-robertblair <ianrobertblair@icloud.com>
2023-03-14 12:35:00 +0800

    Add files from earlier version

---
+++ SocialSocial/CustomErrors.swift
@@ -0,0 +1,10 @@
+//
+//  CustomErrors.swift
+//  SocialSocial
+//
+//  Created by ian robert blair on 2023/2/11.
+//
+
+import Foundation
+
+
---
+++ SocialSocial/Utility/AudioPlayer.swift
@@ -0,0 +1,22 @@
+//
+//  AudioPlayer.swift
+//  SocialSocial
+//
+//  Created by ian robert blair on 2023/3/4.
+//
+
+import Foundation
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print("Could not play the sound file")
+        }
+    }
+}
